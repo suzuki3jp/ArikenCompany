7890ef834c5f291bc868764e5e528275c1b6bf78
suzuki3jp <99058480+suzuki3jp@users.noreply.github.com>
2023-12-04 22:01:31 +0900

    Add Command and Prisma classes for database operations

---
+++ src/database/Command.ts
@@ -0,0 +1,46 @@
+import { Prisma, CommandT } from './Prisma';
+
+export class Command extends Prisma {
+    constructor() {
+        super();
+    }
+
+    async add(name: string, content: string) {
+        const data: CommandT = {
+            name,
+            content,
+        };
+        return await this.prisma.command.create({ data });
+    }
+
+    async editContentById(id: number, content: string) {
+        return await this.prisma.command.update({
+            where: {
+                id,
+            },
+            data: {
+                content,
+            },
+        });
+    }
+
+    async removeById(id: number) {
+        return await this.prisma.command.delete({
+            where: {
+                id,
+            },
+        });
+    }
+
+    async getByName(name: string) {
+        return await this.prisma.command.findFirst({
+            where: {
+                name,
+            },
+        });
+    }
+
+    async getAll() {
+        return await this.prisma.command.findMany();
+    }
+}
---
+++ src/database/Prisma.ts
@@ -0,0 +1,11 @@
+import { Prisma as P, PrismaClient } from '@prisma/client';
+
+export class Prisma {
+    public prisma: PrismaClient;
+
+    constructor() {
+        this.prisma = new PrismaClient();
+    }
+}
+
+export type CommandT = P.CommandCreateInput;
