3ed88e9a45ef76a0eeaa67bd594c9da3b80739f9
suzuki3jp <99058480+suzuki3jp@users.noreply.github.com>
2023-12-04 17:28:12 +0900

    Add toEnv function and Env class for managing environment variables

--- src/packages/index.ts
+++ src/packages/index.ts
@@ -3,3 +3,5 @@ export * from './FileSystem/FileSystem';
 export * from './JST/JST';
 
 export * from './Logger/Logger';
+
+export * from './toEnv/toEnv';
---
+++ src/typings/EnvCache.ts
@@ -0,0 +1,8 @@
+export interface EnvCache {
+    DISCORD_TOKEN: string;
+    TWITCH_CLIENTID: string;
+    TWITCH_CLIENTSECRET: string;
+    TWITCH_TOKEN: string;
+    TWITCH_REFRESHTOKEN: string;
+    DATABASE_URL: string;
+}
