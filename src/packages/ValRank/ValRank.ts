ce04f6fb8bd51d5ff416ef6cc4ea0bd95021d62d
suzuki3jp <99058480+suzuki3jp@users.noreply.github.com>
2023-12-07 21:51:15 +0900

    Update fetch method in ValRank.ts

--- src/packages/ValRank/ValRank.ts
+++ src/packages/ValRank/ValRank.ts
@@ -9,7 +9,7 @@ export class ValRank {
      */
     async get(name: string, tag: string): Promise<string | number> {
         const url = `https://api.henrikdev.xyz/valorant/v1/mmr/ap/${encodeURI(name)}/${encodeURI(tag)}`;
-        const res = await fetch<RankResponse>(url);
+        const res = await fetch(url);
         if (res.status !== 200) return res.status;
         if (!res.data) return 500;
         const data = res.data;
