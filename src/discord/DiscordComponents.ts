6cc96f36ab74b8ef2b31dd6e41af05cdd557b550
suzuki3jp <99058480+suzuki3jp@users.noreply.github.com>
2023-12-09 19:42:15 +0900

    Add DiscordActionRows to DiscordComponents

--- src/discord/DiscordComponents.ts
+++ src/discord/DiscordComponents.ts
@@ -1,4 +1,4 @@
-import { ButtonBuilder, ButtonStyle } from 'discord.js';
+import { ButtonBuilder, ButtonStyle, ActionRowBuilder } from 'discord.js';
 
 export const DiscordComponentIds = {
     button: {
@@ -23,31 +23,30 @@ const DiscordComponentLabels = {
 const previousButton = new ButtonBuilder()
     .setCustomId(DiscordComponentIds.button.previous)
     .setLabel(DiscordComponentLabels.button.previous)
-    .setStyle(ButtonStyle.Primary)
-    .toJSON();
+    .setStyle(ButtonStyle.Primary);
 
 const nextButton = new ButtonBuilder()
     .setCustomId(DiscordComponentIds.button.next)
     .setLabel(DiscordComponentLabels.button.next)
-    .setStyle(ButtonStyle.Primary)
-    .toJSON();
+    .setStyle(ButtonStyle.Primary);
 
 const addButton = new ButtonBuilder()
     .setCustomId(DiscordComponentIds.button.add)
     .setLabel(DiscordComponentLabels.button.add)
-    .setStyle(ButtonStyle.Primary)
-    .toJSON();
+    .setStyle(ButtonStyle.Success);
 
 const editButton = new ButtonBuilder()
     .setCustomId(DiscordComponentIds.button.edit)
     .setLabel(DiscordComponentLabels.button.edit)
-    .setStyle(ButtonStyle.Primary)
-    .toJSON();
+    .setStyle(ButtonStyle.Success);
 
 const removeButton = new ButtonBuilder()
     .setCustomId(DiscordComponentIds.button.remove)
     .setLabel(DiscordComponentLabels.button.remove)
-    .setStyle(ButtonStyle.Primary)
-    .toJSON();
+    .setStyle(ButtonStyle.Danger);
 
 export const DiscordComponents = { previousButton, nextButton, addButton, editButton, removeButton };
+export const DiscordActionRows = {
+    pageController: new ActionRowBuilder<ButtonBuilder>().addComponents(previousButton, nextButton),
+    commandController: new ActionRowBuilder<ButtonBuilder>().addComponents(addButton, editButton, removeButton),
+};
