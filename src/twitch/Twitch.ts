37b0334cfb8de325e67c2af5af77b895b33b46b7
suzuki3jp <99058480+suzuki3jp@users.noreply.github.com>
2023-12-12 15:43:19 +0900

    Add Command class for managing Twitch commands

--- src/twitch/Twitch.ts
+++ src/twitch/Twitch.ts
@@ -3,11 +3,10 @@ import { ChatClient, ChatMessage } from '@twurple/chat';
 import { ApiClient } from '@twurple/api';
 
 import { ArikenCompany } from '../ArikenCompany';
+import { Command } from './Command';
 import { ADD_COMMAND, EDIT_COMMAND, REMOVE_COMMAND, COOLDOWN, SET_COOLDOWN } from '../constants';
-import type { CommandT } from '../database';
-import { CommandManager } from '../managers';
-import { Message, Logger, dayjs } from '../packages';
-import { CommandParser, ValueParser } from '../parsers';
+import { Message, Logger } from '../packages';
+import { ValueParser } from '../parsers';
 
 export class Twitch {
     public auth: RefreshingAuthProvider;
@@ -87,153 +86,38 @@ class Chat {
 
     async onMessage(...args: [string, string, string, ChatMessage]) {
         const message = new Message(this.twitch, ...args);
-        const chat = new TwitchChat(this.twitch.ac, message);
+        const command = new Command(this.twitch.ac, message);
 
-        if (!chat.isCommand()) return;
+        if (!command.isCommand()) return;
 
-        if (chat.isManageCommand()) {
-            if (!chat.isManager()) return;
-            switch (chat.name) {
+        if (command.isManageCommand()) {
+            if (!command.isManager()) return;
+            switch (command.name) {
                 case ADD_COMMAND:
-                    chat.reply(await chat.addCommand());
+                    command.reply(await command.addCommand());
                     break;
                 case EDIT_COMMAND:
-                    chat.reply(await chat.editCommand());
+                    command.reply(await command.editCommand());
                     break;
                 case REMOVE_COMMAND:
-                    chat.reply(await chat.removeCommand());
+                    command.reply(await command.removeCommand());
                     break;
                 case COOLDOWN:
-                    chat.reply(await chat.getCoolDown());
+                    command.reply(await command.getCoolDown());
                     break;
                 case SET_COOLDOWN:
-                    chat.reply(await chat.setCoolDown());
+                    command.reply(await command.setCoolDown());
                     break;
                 default:
                     break;
             }
         } else {
-            const cmd = await chat.normalCommand();
+            const cmd = await command.normalCommand();
             if (!cmd) return;
-            if (!chat.isCooldown(cmd)) return;
+            if (!command.isCooldown(cmd)) return;
             const r = await new ValueParser(this.twitch.ac, cmd.content, message).parse();
-            chat.reply(r.error ?? r.toJSON().parsed);
-            chat.updateUsedAt(cmd.name);
+            command.reply(r.error ?? r.toJSON().parsed);
+            command.updateUsedAt(cmd.name);
         }
-    }
-}
-
-class TwitchChat {
-    public name: string;
-
-    private cmd: CommandManager;
-    private parser: CommandParser;
-
-    constructor(private ac: ArikenCompany, private message: Message) {
-        this.parser = new CommandParser(this.ac, this.message.content);
-        this.cmd = this.ac.cmd;
-        this.name = this.parser.name;
-    }
-
-    isCommand(): boolean {
-        return this.parser.isCommand;
-    }
-
-    isManageCommand(): boolean {
-        const c = this.parser.name;
-        if (c === ADD_COMMAND) return true;
-        if (c === EDIT_COMMAND) return true;
-        if (c === REMOVE_COMMAND) return true;
-        if (c === COOLDOWN) return true;
-        if (c === SET_COOLDOWN) return true;
-        return false;
-    }
-
-    isManager(): boolean {
-        if (this.message.user.isBroadCaster) return true;
-        if (this.message.user.isMod) return true;
-        return false;
-    }
-
-    isVip(): boolean {
-        if (this.message.user.isBroadCaster) return true;
-        if (this.message.user.isMod) return true;
-        if (this.message.user.isVip) return true;
-        return false;
-    }
-
-    async addCommand(): Promise<string> {
-        const { name, content } = this.validateCommandData(this.parser.args);
-
-        if (!(name && content)) return 'コマンドの引数が不正です。コマンド名と内容を入力してください。';
-        return await this.cmd.addCommand(name, content);
-    }
-
-    async editCommand(): Promise<string> {
-        const { name, content } = this.validateCommandData(this.parser.args);
-
-        if (!(name && content)) return `コマンドの引数が不正です。コマンド名と内容を入力してください。`;
-        return await this.cmd.editCommand(name, content);
-    }
-
-    async removeCommand(): Promise<string> {
-        const name = this.validateCommandName(this.parser.args[0]);
-
-        if (!name) return `コマンドの引数が不正です。コマンド名を入力してください。`;
-        return await this.cmd.removeCommand(name);
-    }
-
-    async getCoolDown(): Promise<string> {
-        const name = this.validateCommandName(this.parser.args[0]);
-
-        if (!name) return `コマンドの引数が不正です。コマンド名を入力してください。`;
-        const r = await this.cmd.getCooldown(name);
-        if (!r) return `存在しないコマンド名です。`;
-        return `${name} のクールダウンは ${r} 秒です。`;
-    }
-
-    async setCoolDown(): Promise<string> {
-        const name = this.validateCommandName(this.parser.args[0]);
-        const cooldown = Number(this.parser.args[1]);
-
-        if (!name) return `コマンドの引数が不正です。コマンド名を入力してください。`;
-        if (isNaN(cooldown)) return `クールダウンには数字を入力してください。`;
-        return await this.cmd.setCooldown(name, cooldown);
-    }
-
-    async normalCommand() {
-        const name = this.validateCommandName(this.parser.name);
-        if (!name) return null;
-        return await this.cmd.getCommand(name);
-    }
-
-    reply(content: string) {
-        this.message.reply(content);
-    }
-
-    private validateCommandName(n: string): string | null {
-        if (!n) return null;
-        return n.toLowerCase();
-    }
-
-    public isCooldown(command: CommandT): boolean {
-        if (this.isVip()) return true;
-        const now = dayjs();
-        const lastUsed = dayjs(command.used_at);
-        const cooldown = command.cooldown ?? 0;
-        const cooldownMilli = cooldown * 1000;
-        return now.diff(lastUsed, 'millisecond') > cooldownMilli;
-    }
-
-    public updateUsedAt(name: string) {
-        return this.cmd.updateUsedAt(name);
-    }
-
-    private validateCommandData(args: string[]): { name: string | null; content: string | null } {
-        let [name, ...contents]: (string | null)[] = args;
-        let content: string | null = contents.join(' ');
-        if (!name) name = null;
-        if (!content) content = null;
-        return { name, content };
     }
 }
