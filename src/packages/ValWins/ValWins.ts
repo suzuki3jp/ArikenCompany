f39eeec3c05df23d98a1e6b17027996277c7a070
suzuki3jp <99058480+suzuki3jp@users.noreply.github.com>
2023-12-14 14:00:47 +0900

    Add ValWins class for retrieving Valorant wins

---
+++ src/packages/ValWins/ValWins.ts
@@ -0,0 +1,56 @@
+import { fetch } from '../fetch/fetch';
+
+export class ValWins {
+    constructor() {}
+    /**
+     * Get valorant wins from name and tag
+     * @param {string} name
+     * @param {string} tag
+     */
+    async get(name: string, tag: string): Promise<string | number> {
+        const url = `https://api.henrikdev.xyz/valorant/v3/matches/ap/${encodeURI(name)}/${encodeURI(tag)}`;
+        const res: RankResponse = await fetch(url);
+        if (res.status !== 200) return res.status;
+        if (!res.data) return 500;
+        const data = res.data;
+        return this.parseWins(data, name, tag);
+    }
+
+    private parseWins(data: RankResponse['data'], name: string, tag: string): string {
+        let wins = 0;
+        data?.forEach((m) => {
+            const wonTeam = m.teams.red.has_won ? 'red' : 'blue';
+            const isPlayerWon =
+                m.players.all_players.find((p) => p.name === name && p.tag === tag)?.team.toLowerCase() === wonTeam;
+            if (isPlayerWon) wins++;
+        });
+        return `コンペ直近5試合の勝率: ${wins}W${5 - wins}L`;
+    }
+}
+
+interface RankResponse {
+    status: number;
+    data?: MatchData[];
+    // エラーは必要ないため、anyで定義
+    errors?: any[];
+}
+
+interface MatchData {
+    players: {
+        all_players: MatchPlayerData[];
+    };
+    teams: {
+        red: {
+            has_won: boolean;
+        };
+        blue: {
+            has_won: boolean;
+        };
+    };
+}
+
+interface MatchPlayerData {
+    name: string;
+    tag: string;
+    team: 'Red' | 'Blue';
+}
