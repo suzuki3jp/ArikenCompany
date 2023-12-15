2333aced9d2e922b67f52f253678048097f6276a
suzuki3jp <99058480+suzuki3jp@users.noreply.github.com>
2023-12-16 04:52:45 +0900

    Add user registration service implementation

---
+++ src/api/routes/user/register/index.ts
@@ -0,0 +1,15 @@
+import type { Request, Response } from 'express';
+
+import { RouteImpl } from '../../../RouteImpl';
+import { Api } from '../../../Api';
+
+export class RegisterService implements RouteImpl {
+    constructor(public path: string, public api: Api) {}
+
+    public async post(req: Request, res: Response) {
+        const { name, password } = req.body;
+        const addUserResult = (await this.api.ac.um.add(name, password)).toJSON();
+        if (addUserResult.status === 200) this.api.logger.info('User added ' + addUserResult.data?.name);
+        res.status(addUserResult.status).json(addUserResult);
+    }
+}
