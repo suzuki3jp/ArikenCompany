da5765e0f489021478a9e22a00bf9a5797eda235
suzuki3jp <99058480+suzuki3jp@users.noreply.github.com>
2023-12-16 18:49:13 +0900

    Add validation for missing name or password in user registration

--- src/api/routes/user/register/index.ts
+++ src/api/routes/user/register/index.ts
@@ -2,6 +2,7 @@ import type { Request, Response } from 'express';
 
 import { RouteBase } from '../../RouteBase';
 import { Api } from '../../../Api';
+import { HttpResult } from '../../../../packages';
 
 export class RegisterService implements RouteBase {
     public static path = '/user/register';
@@ -10,6 +11,11 @@ export class RegisterService implements RouteBase {
 
     public async post(req: Request, res: Response) {
         const { name, password } = req.body;
+        if (!name || !password) {
+            const r = new HttpResult().setStatus(400).setMessage('Missing name or password').toJSON();
+            res.status(r.status).json(r);
+            return;
+        }
         const addUserResult = (await this.api.ac.um.add(name, password)).toJSON();
         if (addUserResult.status === 200) this.api.logger.info('User added ' + addUserResult.data?.name);
         res.status(addUserResult.status).json(addUserResult);
