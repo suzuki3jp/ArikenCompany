e4da61668c73eefcaedd26ba589dbdc3bce7c3b9
suzuki3jp <99058480+suzuki3jp@users.noreply.github.com>
2024-01-17 20:13:44 +0900

    page: Change to use mui.

--- pages/src/App.tsx
+++ pages/src/App.tsx
@@ -1,12 +1,19 @@
-import './App.css';
+import { Box, CssBaseline, createTheme, ThemeProvider, Typography } from '@mui/material';
+
+const darkTheme = createTheme({
+    palette: {
+        mode: 'dark',
+    },
+});
 
 function App() {
     return (
-        <div className='App'>
-            <header className='App-header'>
-                <div>準備中</div>
-            </header>
-        </div>
+        <ThemeProvider theme={darkTheme}>
+            <Box sx={{ display: 'flex', justifyContent: 'center', marginTop: '10%' }}>
+                <CssBaseline></CssBaseline>
+                <Typography variant='h2'>準備中</Typography>
+            </Box>
+        </ThemeProvider>
     );
 }
 
