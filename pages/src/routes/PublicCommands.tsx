d7b771939482ee27449a6453b25331ddca92d318
suzuki3jp <99058480+suzuki3jp@users.noreply.github.com>
2024-01-24 00:08:34 +0900

    pages: Add route `PublicCommands`.

---
+++ pages/src/routes/PublicCommands.tsx
@@ -0,0 +1,67 @@
+import { Component, ReactNode } from 'react';
+import { Grid, Card } from '@mui/material';
+
+import { ApiClient, PublicCommandData } from '../api-wrapper';
+import { Loading, PublicCommandsTable, Title } from '../components';
+
+export class PublicCommands extends Component<PublicCommandsProps, PublicCommandsState> {
+    constructor(props: PublicCommandsProps) {
+        super(props);
+        this.state = {
+            uptime: null,
+            publicCommands: null,
+        };
+    }
+
+    async fetchStatus() {
+        const { uptime } = await new ApiClient().getStatus();
+        this.setState({ uptime });
+    }
+
+    async fetchPublicCommands() {
+        const c = await new ApiClient().getPublicCommands();
+        this.setState({ publicCommands: c });
+    }
+
+    componentDidMount(): void {
+        this.fetchStatus();
+        this.fetchPublicCommands();
+    }
+
+    render(): ReactNode {
+        return (
+            <>
+                <Grid container spacing={3}>
+                    <Grid item xs={12}>
+                        <Card
+                            sx={{
+                                p: 2,
+                                display: 'flex',
+                                flexDirection: 'column',
+                                height: 'auto',
+                            }}
+                        >
+                            <Title>ArikenCompany Commands</Title>
+                            {this.state.publicCommands ? (
+                                <PublicCommandsTable cmds={this.state.publicCommands}></PublicCommandsTable>
+                            ) : (
+                                <Loading></Loading>
+                            )}
+                        </Card>
+                    </Grid>
+                </Grid>
+            </>
+        );
+    }
+}
+
+export interface PublicCommandsProps {}
+
+export interface PublicCommandsState {
+    uptime: {
+        hours: number;
+        minutes: number;
+        seconds: number;
+    } | null;
+    publicCommands: PublicCommandData[] | null;
+}
