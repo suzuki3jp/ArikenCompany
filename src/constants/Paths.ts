bf17f39bbc2aac249305e86f0a5ea0bcab780d8a
suzuki3jp <suzuki3.info@gmail.com>
2024-02-20 21:05:03 +0900

    core: ログをファイルに保存するように変更

--- src/constants/Paths.ts
+++ src/constants/Paths.ts
@@ -5,6 +5,8 @@ const resolvePath = (path: string) => resolve(__dirname, path);
 export class Path {
     public static readonly env: string = resolvePath('../../.env');
     public static readonly settings: string = resolvePath('../../data/settings.json');
+    public static readonly prodLog: string = resolvePath('../../data/prod.log');
+    public static readonly debugLog: string = resolvePath('../../data/debug.log');
     public static readonly cert: string = resolvePath('/etc/letsencrypt/live/api.suzuki3jp.xyz/fullchain.pem');
     public static readonly key: string = resolvePath('/etc/letsencrypt/live/api.suzuki3jp.xyz/privkey.pem');
 }
