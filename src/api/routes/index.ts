6b06de8cd0e38ccce8c340a6dcc9e78d51a0dd5a
suzuki3jp <suzuki3.info@gmail.com>
2024-03-17 16:03:07 +0900

    feat(api): `UserMeService`を追加

--- src/api/routes/index.ts
+++ src/api/routes/index.ts
@@ -1,3 +1,4 @@
 import { LoginService } from '@/api/routes/auth/LoginService';
+import { UserMeService } from '@/api/routes/user/me/UserMeService';
 
-export const services = [LoginService] as const;
+export const services = [LoginService, UserMeService] as const;
