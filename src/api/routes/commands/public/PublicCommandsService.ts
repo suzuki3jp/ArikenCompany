0f58ad71c5cc7b891065f4dad1b9e6bdbaaeb30e
suzuki3jp <suzuki3.info@gmail.com>
2024-03-23 16:19:23 +0900

    feat(api): `PublicCommandsService`を追加

---
+++ src/api/routes/commands/public/PublicCommandsService.ts
@@ -0,0 +1,41 @@
+import { HttpStatusCode } from 'axios';
+import type { Request, Response } from 'express';
+
+import { RouteT } from '@/api/RouteLoader';
+import type { ArikenCompany } from '@/ArikenCompany';
+import { BaseErrorRes, BaseRes, ErrorCode, MiddlewareUtils } from '@/api/utils';
+import { PublicUserData } from '@/managers';
+import { Logger } from '@/packages';
+import { rootLogger } from '@/initializer';
+
+export class PublicCommandsService implements RouteT {
+    public readonly path = '/commands/public';
+    public readonly requiredRole: RouteT['requiredRole'] = { get: null, post: null, put: null, delete: null };
+
+    private logger: Logger = rootLogger.createChild('PublicCommandsService');
+
+    constructor(private ac: ArikenCompany) {}
+
+    async get(req: Request, res: Response) {
+        const commands = await this.ac.cmd.getAll();
+
+        const publicCommands = commands.map<PublicCommandData>((c) => ({
+            name: c.name,
+            content: c.content,
+            cooldown: c.cooldown,
+            mod_only: c.mod_only,
+            alias: c.alias ?? undefined,
+        }));
+
+        res.status(HttpStatusCode.Accepted).json(publicCommands);
+        return;
+    }
+}
+
+export interface PublicCommandData {
+    name: string;
+    content: string;
+    cooldown: number;
+    mod_only: boolean;
+    alias?: string;
+}
